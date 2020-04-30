383f548dd250080c869fb92813bd23dd1bc0498c
Lucas Pains <lpains@jbssa.com>
2020-04-30 15:21:10 -0600

    missed updating dist folder

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,13 @@ export const mouseless = {
         const focus = binding && binding.modifiers && binding.modifiers.focus;
         // el might not be present for server-side rendering.
         if (el) {
-            MouseTrap.bind(binding.value, (e) => {
+            MouseTrap.bind(binding.value instanceof Object ? binding.value.key : binding.value, (e) => {
                 if (focus) {
                     el.focus();
+                }
+                else if (binding.value instanceof Object &&
+                    binding.value.action instanceof Function) {
+                    binding.value.action();
                 }
                 else {
                     el.click();
